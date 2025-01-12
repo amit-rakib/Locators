a04ece6af11da54a4287864f62ad8c03a91c6083
amit <mr.ahr.es6@gmail.com>
2025-01-12 09:03:21 +0600

    Cross Browser Class Added

---
+++ src/amit/CrossBrowser.java
@@ -0,0 +1,64 @@
+package amit;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class CrossBrowser {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		
+		//Chrome Browser
+		//WebDriver driver = new ChromeDriver();
+		
+		//Firefox Browser
+		//WebDriver driver = new FirefoxDriver();
+		
+		//Edge Browser
+		WebDriver driver = new EdgeDriver();
+		
+		driver.get("https://rahulshettyacademy.com/locatorspractice/");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(7));
+		
+		driver.findElement(By.linkText("Forgot your password?")).click();
+		Thread.sleep(3000);
+		driver.findElement(By.cssSelector("button.reset-pwd-btn")).click();
+		
+		
+		//Please use temporary password 'rahulshettyacademy' to Login.
+		
+		String text = driver.findElement(By.cssSelector("p.infoMsg")).getText();
+		
+		
+		String[] text2 = text.split("'");
+		
+		// 0th index - Please use temporary password 
+		// 1th index - rahulshettyacademy' to Login.
+		
+		String password = text2[1].split("'")[0];
+		
+		// 0th index - rahulshettyacademy
+		// 1th index -  to Login.
+		
+		driver.findElement(By.xpath("//*[text()='Go to Login']")).click();
+		driver.findElement(By.xpath("//input[@placeholder='Username']")).sendKeys("Amit");
+		driver.findElement(By.xpath("//input[@placeholder='Password']")).sendKeys(password);
+		Thread.sleep(3000);
+
+		driver.findElement(By.id("chkboxOne")).click();
+		driver.findElement(By.id("chkboxTwo")).click();
+		driver.findElement(By.className("signInBtn")).click();
+		
+		String successMsg = driver.findElement(By.xpath("//*[.='You are successfully logged in.']")).getText();
+		System.out.println(successMsg);
+		
+		driver.close();
+
+	}
+
+}
